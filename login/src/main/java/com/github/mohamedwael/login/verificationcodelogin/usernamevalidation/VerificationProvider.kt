72b155470e3a9cd0a6c38c72fa320bee61824025
Mohamed Wael <imohamedwael@outlook.com>
2020-06-23 01:48:12 +0200

    adding sms code to VerificationProvider

--- login/src/main/java/com/github/mohamedwael/login/verificationcodelogin/usernamevalidation/VerificationProvider.kt
+++ login/src/main/java/com/github/mohamedwael/login/verificationcodelogin/usernamevalidation/VerificationProvider.kt
@@ -3,5 +3,5 @@ package com.github.mohamedwael.login.verificationcodelogin.usernamevalidation
 import com.blogspot.mowael.baselibrary.contract.ErrorMessageHandler
 
 interface VerificationProvider {
-    fun sendVerificationCode(userName: String, onSuccess: () -> Unit, onError: (ErrorMessageHandler) -> Unit)
+    fun sendVerificationCode(userName: String, onSuccess: (smsCode:String) -> Unit, onError: (ErrorMessageHandler) -> Unit)
 }
