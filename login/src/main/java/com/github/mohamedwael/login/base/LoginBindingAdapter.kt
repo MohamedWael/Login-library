6343589b274819cd4a755b8ab8ed327b46bf1340
Mohamed Wael <imohamedwael@outlook.com>
2020-07-02 16:33:47 +0200

    fixing underline text binding adapter issue

--- login/src/main/java/com/github/mohamedwael/login/base/LoginBindingAdapter.kt
+++ login/src/main/java/com/github/mohamedwael/login/base/LoginBindingAdapter.kt
@@ -8,38 +8,21 @@ import com.google.android.material.textfield.TextInputLayout
 
 object LoginBindingAdapter {
 
-//    @JvmStatic
-//    @BindingAdapter("app:textUnderline")
-//    fun textUnderline(textView: TextView, strRes: Int) {
-//        val content = SpannableString(textView.context.getString(strRes))
-//        content.setSpan(UnderlineSpan(), 0, content.length, 0)
-//        textView.text = textView.context.getString(strRes)
-//    }
-
-//    @JvmStatic
-//    @BindingAdapter("app:textUnderline")
-    fun setUnderlinedText(textView: TextView, strRes: Int) {
-        val underlineSpan = SpannableString(textView.context.getString(strRes))
-        underlineSpan.setSpan(UnderlineSpan(), 0, underlineSpan.length, 0)
-        textView.text = underlineSpan
+    @JvmStatic
+    @BindingAdapter("app:textUnderline")
+    fun textUnderline(textView: TextView, strRes: Int) {
+        val content = SpannableString(textView.context.getString(strRes))
+        content.setSpan(UnderlineSpan(), 0, content.length, 0)
+        textView.text = textView.context.getString(strRes)
     }
 
     @JvmStatic
     @BindingAdapter("app:textUnderline")
-    fun setUnderlinedText(textView: TextView, text: String) {
+    fun textUnderline(textView: TextView, text: String) {
         val underlineSpan = SpannableString(text)
         underlineSpan.setSpan(UnderlineSpan(), 0, underlineSpan.length, 0)
         textView.text = underlineSpan
     }
-
-//    @JvmStatic
-//    @BindingAdapter("app:textUnderline")
-//    fun textUnderline(textView: TextView, text: String) {
-//        val content = SpannableString(text)
-//        content.setSpan(UnderlineSpan(), 0, content.length, 0)
-//        textView.text = text
-//    }
-
 
     @JvmStatic
     @BindingAdapter("app:errorText")
